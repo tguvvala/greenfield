0773ac23e45f4a2a9d68bb7b74ab1158f178c74e
Tejaswi <tejaswi.reddy6@gmail.com>
2017-11-20 22:19:29 -0800

    Added database-spec file

--- database/index.js
+++ database/index.js
@@ -1,14 +1,9 @@
 const mysql = require('mysql');
 
-
 // Create a db connection & export it from this file
-
 // need to connect to user 'root', no password, and to db 'choreApp'
-
-
-const dbConnection = mysql.createConnection({
+let dbConnection = mysql.createConnection({
   user: 'root',
-  password: '',
   database: 'choreApp'
 });
 
